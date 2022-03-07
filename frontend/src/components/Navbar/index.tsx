7f537e2246714941e7c3c44bdd0a32ad2e94aa45
Matheus Trindade <matheus.t.protasio@gmail.com>
2022-03-07 13:59:27 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import {ReactComponent as GithubIcon} from "../../Assets/img/github_logo.svg";
+import "./styles.css";
+
+function NavBar () {
+    return(
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DS MOVIE</h1>
+          <a href="https://github.com/mtpTrindade/dsmovie">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/Matheus Trindade</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+    );
+
+}
+
+export default NavBar;
